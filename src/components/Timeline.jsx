a7f74b6d4ba0bfc6f93fc11207a84db6c4ada4f0
zren45 <zhixiangren@gatech.edu>
2021-12-17 13:16:38 -0600

    github desktop test

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./Timeline.css";
 
@@ -50,56 +49,3 @@ function Timeline(props) {
 }
 
 export default Timeline;
-=======
-import React from "react";
-import "./Timeline.css";
-
-function Timeline(props) {
-  return (
-    <div className="timeline__container">
-      <div className="timeline__buttons__container">
-        <button
-          className="timeline__button "
-          onClick={() => {
-            props.childToParent("1d");
-          }}
-        >
-          1D
-        </button>
-        <button
-          className="timeline__button "
-          onClick={() => props.childToParent("5d")}
-        >
-          5D
-        </button>
-        <button
-          className="timeline__button"
-          onClick={() => props.childToParent("1m")}
-        >
-          1M
-        </button>
-        <button
-          className="timeline__button"
-          onClick={() => props.childToParent("3m")}
-        >
-          3M
-        </button>
-        <button
-          className="timeline__button"
-          onClick={() => props.childToParent("1y")}
-        >
-          1Y
-        </button>
-        <button
-          className="timeline__button"
-          onClick={() => props.childToParent("5y")}
-        >
-          5Y
-        </button>
-      </div>
-    </div>
-  );
-}
-
-export default Timeline;
->>>>>>> fb1e426982acb339acff623b0fcf145be187d86c
